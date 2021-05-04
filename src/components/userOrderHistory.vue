4736209eabb84fbfa31551eb50bd2b467662e33f
Farhan Rabbaanii <farhanrbnn@gmail.com>
2021-05-05 05:47:26 +0700

    update on user order history

--- src/components/userOrderHistory.vue
+++ src/components/userOrderHistory.vue
@@ -4,22 +4,14 @@
 		<b-container class="mt-5">
 			<h3 class="no-items" v-if="orderData.length === 0">You're not order anything yet</h3>
 			<b-card v-if="orderData" v-for="(data, idx) in orderData" :key="idx">
-        <b-row>
-        	<b-col md="4">
-            <b-card-img id="product-image" :src="data.picture" alt="Image" class="rounded-0"  style="max-width: 10rem; height: auto;"></b-card-img>
-          </b-col>
-          <b-col md="8">
-          	<b-card-body>
-            	<b-card-text>
-             		<div class="float-left">
-                	<h5>{{data.name}}</h5>
-                    <!-- <h5>{{data.quantity}} X Rp. {{data.price}}</h5> -->
-                    <!-- <h5 class="mt-4">Subtotal: Rp. {{data.subTotal}}</h5> -->
-                </div>
-              </b-card-text>
-            </b-card-body>
-          </b-col>
-        </b-row>
+				<b-row v-if="data.item.length === 1" v-for="(item, idx) in data.item" :key="idx">
+					<b-col md="4">
+						<b-card-img id="product-image" :src="item.picture" alt="Image" class="rounded-0"  style="max-width: 10rem; height: auto;"></b-card-img>
+					</b-col>
+					<b-col md="8">
+						<h5>{{item.name}}</h5>
+					</b-col>
+				</b-row>
 			</b-card>			
 		</b-container>
 	</div>
@@ -47,9 +39,8 @@ export default {
 
 		DataService.get(url)
 		.then((res) => {
-			// this.orderData.push(res.data.data.item)
-			this.orderData = res.data.data.item
-			console.log(this.orderData)
+			const apiData = res.data.data
+			this.orderData = apiData
 		})
 		.catch((err) => {
 			console.log(err)
